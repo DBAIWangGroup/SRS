0dfe9cd6917d60383624acfb669bd55047e03d60
Yifang Sun <yifang.bill.sun@gmail.com>
2015-05-22 11:48:19 +1000

    First commit

---
+++ run_hard_data.sh
@@ -0,0 +1,31 @@
+#!/bin/sh                                                                                                                                                                                                                                                                
+
+
+# Check dependancy.                                                                                                                                                                                                                                                      
+
+echo "== Compiling...  =="
+make clean
+
+make all
+
+echo " "
+echo "== Calculating parameter =="
+./cal_param -n 10000 -m 7
+
+if [ -d "index" ]; then
+    rm -r index
+fi
+mkdir index
+
+echo " "
+echo "== Testing using different random seeds =="
+for i in 1 2 3 4 5 6 7 8 9 10
+do
+    #echo " "
+    echo "= Test $i ="
+    ./srs -I -d 128 -i index/ -m 7 -n 10000 -s hard_data/hard.ds -y f -e $i
+    
+    #echo " "
+    #echo "== Processing query workload... =="
+    ./srs -Q -c 4 -g hard_data/hard.gt -i index/ -k 1 -q hard_data/hard.q -t 6 -r 0.299203
+done
---
+++ run_toy_data.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+
+# Check dependancy. 
+
+echo "== Compiling...  =="
+make clean
+
+make all
+
+echo " "
+echo "== Calculating parameter =="
+./cal_param -n 3000 -m 7
+
+echo " "
+echo "== Generating ground truth =="
+./gen_gt -d 192 -n 3000 -k 10 -q data/toy.q -s data/toy.ds -g data/toy.gt -y i
+
+if [ -d "index" ]; then
+    rm -r index
+fi
+mkdir index
+
+echo " "
+echo "== Indexing... =="
+./srs -I -d 192 -i index/ -m 7 -n 3000 -s data/toy.ds 
+
+echo " "
+echo "== Processing query workload... =="
+./srs -Q -c 4 -g data/toy.gt -i index/ -k 10 -q data/toy.q -t 2 -r 0.299203
